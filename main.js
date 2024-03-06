89a8e9396411174656be2c6879deaf773329413b
Jonathan <jonathancarranza70@gmail.com>
2024-03-05 23:13:24 -0300

    first commiteo

---
+++ main.js
@@ -0,0 +1,11 @@
+//vanilla javaScript
+
+//recuperamos el botón
+const button = document.querySelector('button')
+
+//al hacer click en el botón, tenemos que ejecutar una función
+button.addEventListener('click', function()
+{
+    alert('Hola')
+}
+)
