e257a96ae3f764ba8be2445b6a605450f942258d
Jonathan <jonathancarranza70@gmail.com>
2024-03-06 23:03:59 -0300

    un poco de vanilla js

--- main.js
+++ main.js
@@ -6,6 +6,17 @@ const button = document.querySelector('button')
 //al hacer click en el botón, tenemos que ejecutar una función
 button.addEventListener('click', function()
 {
-    alert('Hola')
+    //recuperar la id del atributo del HTML
+    const id = button.getAttribute('data-id')
+
+    //llamar a un servicio para actualizar si me gusta
+    //toggleLike(id)
+    if(button.classList.contains('liked')) {
+        button.classList.remove('liked')
+        button.innerText = 'Me gusta'
+    } else {
+        button.classList.add('liked')
+        button.innerText = 'Quitar me gusta'
+    }
 }
 )
