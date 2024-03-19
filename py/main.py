9368ccde3325b94d916ed596538bd57dc604a96f
Jonathan <jonathancarranza70@gmail.com>
2024-03-18 23:59:19 -0300

    comomomo

---
+++ py/main.py
@@ -0,0 +1,11 @@
+print('extrañaste python?')
+respuesta = input('contame que sentiste? :')
+if respuesta == 'nada':
+    print('sos horrible')
+else:
+    print('me encanta escucharte decir eso! \nYo tambien estoy contento de que estes aqui')
+y_ahora = input('Y ahora? ahora a q jugamos? trajiste algo para mi? : ')
+if y_ahora == 'no':
+    print('como q nada? \n te parece bien no traerme nada? ')
+else:
+    print('wow genial gracias!!')
